fd0a75bb86b48d46336c1f9505a7abf8c9499e93
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 16:52:58 +0100

    feat: :sparkles: Implement store in BuyButton

--- src/components/BuyButton/index.tsx
+++ src/components/BuyButton/index.tsx
@@ -2,14 +2,20 @@ import './BuyButton.scss';
 
 import { FunctionComponent } from 'react';
 
+import { useAppDispatch } from '../../store/hooks';
+import { buyTool } from '../../store/shopSlice';
 import Button from '../shared/Button';
 
-const BuyButton: FunctionComponent<{}> = () => (
-  <Button
-    title="Buy"
-    data-testid="buy-button"
-    className="BuyButton box green text-shadow"
-  />
-);
+const BuyButton: FunctionComponent<{}> = () => {
+  const dispatch = useAppDispatch();
+  return (
+    <Button
+      title="Buy"
+      data-testid="buy-button"
+      className="BuyButton box green text-shadow"
+      onClick={() => dispatch(buyTool())}
+    />
+  );
+};
 
 export default BuyButton;
