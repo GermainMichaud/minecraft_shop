28ab5f82976cc0e4ff49806c2995012aa28078d9
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 16:50:51 +0100

    feat: :sparkles: Implement store in ToolDetail

--- src/components/ToolDetail/index.tsx
+++ src/components/ToolDetail/index.tsx
@@ -2,27 +2,41 @@ import './ToolDetail.scss';
 
 import { FunctionComponent } from 'react';
 
+import { useAppSelector } from '../../store/hooks';
+import { selectCurrentTool, selectSpritePath } from '../../store/shopSlice';
 import Box from '../shared/Box';
 import ImageFromSprite from '../shared/ImageFromSprite';
 
-const ToolDetail: FunctionComponent<{}> = () => (
-  <Box className="ToolDetail" data-testid="tool-detail">
-    <div className="ToolDetail__title">Tool name</div>
-    <div className="ToolDetail__body">
-      <ImageFromSprite />
-      <div className="ToolDetail__body__details">
-        <div className="ToolDetail__body__details__row">
-          INTEGRITY <span>24</span>
-        </div>
-        <div className="ToolDetail__body__details__row">
-          DAMAGE <span>+62</span>
-        </div>
-        <div className="ToolDetail__body__details__row">
-          COST <span>121</span>
+// Image size hardcoded...
+const IMG_SIZE = 80;
+
+const ToolDetail: FunctionComponent<{}> = () => {
+  const currentTool = useAppSelector(selectCurrentTool);
+  const spritePath = useAppSelector(selectSpritePath);
+  return (
+    <Box className="ToolDetail" data-testid="tool-detail">
+      <div className="ToolDetail__title">{currentTool.name}</div>
+      <div className="ToolDetail__body">
+        <ImageFromSprite
+          position={currentTool.spritePosition}
+          width={IMG_SIZE}
+          height={IMG_SIZE}
+          sprite={spritePath}
+        />
+        <div className="ToolDetail__body__details">
+          <div className="ToolDetail__body__details__row">
+            INTEGRITY <span>{currentTool.integrity}</span>
+          </div>
+          <div className="ToolDetail__body__details__row">
+            DAMAGE <span>+{currentTool.damage}</span>
+          </div>
+          <div className="ToolDetail__body__details__row">
+            COST <span>{currentTool.cost}</span>
+          </div>
         </div>
       </div>
-    </div>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default ToolDetail;
