4b19a6ba6e515c3779ba07132448fbec8a088ce0
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 16:40:51 +0100

    feat: :sparkles: Caroussel component done

--- src/components/Caroussel/ListImages.tsx
+++ src/components/Caroussel/ListImages.tsx
@@ -1,12 +1,37 @@
 import { FunctionComponent } from 'react';
 
+import { useAppSelector } from '../../store/hooks';
+import { selectCurrentIndex, selectSpritePath, selectTools } from '../../store/shopSlice';
 import ImageFromSprite from '../shared/ImageFromSprite';
+// import ImageFromSprite from '../shared/ImageFromSprite';
+
+// Size hardcoded...
+const IMG_SIZE = 80;
 
 const ListImages: FunctionComponent<{}> = () => {
+  const tools = useAppSelector(selectTools);
+  const currentIndex = useAppSelector(selectCurrentIndex);
+  const spritePath = useAppSelector(selectSpritePath);
   return (
-    <div className="list-images">
-      {/* Map items */}
-      <ImageFromSprite />
+    <div
+      className="list-images"
+      style={{
+        // Slide the div based on the current index and all passed gap space between images. And for center the image, with have to substract half of the image size.
+        // eslint-disable-next-line prettier/prettier
+        transform: `translateX(calc(${-currentIndex * IMG_SIZE}px - ${IMG_SIZE / 2}px - ${currentIndex}rem))`,
+        // The width is based on the number of images and the gap space between images.
+        width: `calc(${tools.length * IMG_SIZE}px + ${tools.length - 1}rem)`,
+      }}
+    >
+      {tools.map((tool) => (
+        <ImageFromSprite
+          key={tool.key}
+          position={tool.spritePosition}
+          width={IMG_SIZE}
+          height={IMG_SIZE}
+          sprite={spritePath}
+        />
+      ))}
     </div>
   );
 };
--- src/components/Caroussel/index.tsx
+++ src/components/Caroussel/index.tsx
@@ -2,20 +2,31 @@ import './Caroussel.scss';
 
 import { FunctionComponent } from 'react';
 
+import { useAppDispatch } from '../../store/hooks';
+import { nextTool, previousTool } from '../../store/shopSlice';
 import Box from '../shared/Box';
 import Button from '../shared/Button';
 import ListImages from './ListImages';
 
 const Caroussel: FunctionComponent = () => {
+  const dispatch = useAppDispatch();
   return (
     <>
-      <Button title="Prev" className="CarousselButton-prev text-shadow" />
+      <Button
+        title="Prev"
+        className="CarousselButton-prev text-shadow"
+        onClick={() => dispatch(previousTool())}
+      />
       <Box className="Caroussel" data-testid="caroussel">
         <div className="Caroussel__body">
           <ListImages />
         </div>
       </Box>
-      <Button title="Next" className="CarousselButton-next text-shadow" />
+      <Button
+        title="Next"
+        className="CarousselButton-next text-shadow"
+        onClick={() => dispatch(nextTool())}
+      />
     </>
   );
 };
