9325343e0740cd8f4b3fd67d1ae85ff4754cd39a
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 16:55:55 +0100

    feat: :sparkles: Implement store in Wallet

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -2,13 +2,18 @@ import './Wallet.scss';
 
 import { FunctionComponent } from 'react';
 
+import { useAppSelector } from '../../store/hooks';
+import { selectWallet } from '../../store/shopSlice';
 import Box from '../shared/Box';
 
-const Wallet: FunctionComponent<{}> = () => (
-  <Box className="Wallet yellow" data-testid="wallet">
-    <div className="Wallet__title">Wallet</div>
-    <div className="Wallet__body">1500</div>
-  </Box>
-);
+const Wallet: FunctionComponent<{}> = () => {
+  const wallet = useAppSelector(selectWallet);
+  return (
+    <Box className="Wallet yellow" data-testid="wallet">
+      <div className="Wallet__title">Wallet</div>
+      <div className="Wallet__body">{wallet}</div>
+    </Box>
+  );
+};
 
 export default Wallet;
