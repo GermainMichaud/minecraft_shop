a24f194d8c0a0dfa8ed59f9b76a1c6f07138e7a0
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 16:26:09 +0100

    feat: :art: ImageFromProps component done

--- src/components/shared/ImageFromSprite/index.tsx
+++ src/components/shared/ImageFromSprite/index.tsx
@@ -1,7 +1,32 @@
 import { FunctionComponent } from 'react';
 
-const ImageFromSprite: FunctionComponent<{}> = () => {
-  return <div className="image"></div>;
+interface IImageFromSpriteProps {
+  sprite: string;
+  width: number;
+  height: number;
+  position: {
+    x: number;
+    y: number;
+  };
+}
+
+const ImageFromSprite: FunctionComponent<IImageFromSpriteProps> = ({
+  sprite,
+  width,
+  height,
+  position,
+}) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${sprite})`,
+        backgroundPosition: `-${position.x * width}px -${position.y * height}px`,
+        backgroundRepeat: 'no-repeat',
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    />
+  );
 };
 
 export default ImageFromSprite;
