0fae9d64f6b16231090e14e46933accf88300573
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 15:19:12 +0100

    feat: :lipstick: Styling boxes

--- src/components/BuyButton/index.tsx
+++ src/components/BuyButton/index.tsx
@@ -5,7 +5,7 @@ import { FunctionComponent } from 'react';
 import Button from '../shared/Button';
 
 const BuyButton: FunctionComponent<{}> = () => (
-  <Button title="Buy" data-testid="buy-button" className="BuyButton" />
+  <Button title="Buy" data-testid="buy-button" className="BuyButton box green" />
 );
 
 export default BuyButton;
--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -5,7 +5,7 @@ import { FunctionComponent } from 'react';
 import Box from '../shared/Box';
 
 const Wallet: FunctionComponent<{}> = () => (
-  <Box className="Wallet" data-testid="wallet">
+  <Box className="Wallet yellow" data-testid="wallet">
     <div className="Wallet__title">Wallet</div>
     <div className="Wallet__body">1500</div>
   </Box>
