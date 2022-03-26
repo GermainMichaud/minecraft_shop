7dfffb58e556b619a7bdf526165d6bd4cd24da8a
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 13:40:56 +0100

    feat: :tada: Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react'
+import './App.scss'
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
