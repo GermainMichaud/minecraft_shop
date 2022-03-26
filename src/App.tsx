1a0145719eb39e260ab65efaa0a628c6699b18ee
GermainMichaud <gmichaud.dev@gmail.com>
2022-03-26 15:05:59 +0100

    feat: :art: Add components & base style of structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,22 @@ import './App.scss';
 
 import { FunctionComponent } from 'react';
 
+import BuyButton from './components/BuyButton';
+import Caroussel from './components/Caroussel';
+import ToolDetail from './components/ToolDetail';
+import Wallet from './components/Wallet';
+
 const App: FunctionComponent = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Caroussel />
+        <Wallet />
+        <ToolDetail />
+        <BuyButton />
+      </div>
+    </div>
+  );
 };
 
 export default App;
